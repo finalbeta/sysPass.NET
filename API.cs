8b4bc432067f86a47f98ec8bcdba8f3ad35852af
Finalbeta <Finalbeta@finalbeta.net>
2017-04-25 22:53:01 +0200

    Add project files.

---
+++ API.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Dynamic;
+
+namespace sysPassApi
+{
+    public class sysPass
+    {
+        public sysPass(string APIURL, string APIToken, string BasicAuthUser, string BasicAuthPass)
+        {
+            this.user = BasicAuthUser;
+            this.password = BasicAuthPass;
+            this.APIURL = APIURL;
+            this.APIToken = APIToken;
+            if (user != null) this.basicAuth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(this.user + ":" + this.password));
+        }
+
+        private string user = null;
+        private string password;
+        private string APIURL;
+        private string APIToken = null;
+        private string basicAuth = null;
+        
+        public string jsonResponse(string method, dynamic parameters)
+        {
+            string jsonParams = null;
+            if (parameters != null)
+            {
+                jsonParams = JsonConvert.SerializeObject(buildRequest(method, parameters));
+            }
+            return sendRequest(jsonParams);
+        }
+
+
+        public ExpandoObject objectResponse(string method, ExpandoObject parameters)
+        {
+            string jsonParams = null;
+            if (parameters != null)
+            {
+                jsonParams = JsonConvert.SerializeObject(buildRequest(method,parameters));
+            }
+            string jsonResponse = sendRequest(jsonParams);
+            ExpandoObject response = JsonConvert.DeserializeObject<ExpandoObject>(jsonResponse);
+            return response;
+        }
+
+        private string sendRequest(string jsonParams)
+        {
+            WebRequest request = WebRequest.Create(APIURL);
+            if (basicAuth != null) request.Headers.Add("Authorization", "Basic " + basicAuth);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(jsonParams);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            
+            string jsonResult;
+            WebResponse response = request.GetResponse();
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                jsonResult = streamReader.ReadToEnd();
+            }
+            return jsonResult;
+        }
+
+        private object buildRequest(string method, ExpandoObject parameters )
+        {
+            ((dynamic)parameters).authToken = this.APIToken;
+            return new { jsonrpc = "2.0", method = method, id = "sysPass.NET", @params = parameters};
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# sysPass.NET
+**sysPass** is .NET sysPass API library which is incredible easy to use thanks to C# dynamic objects.
+
+Example (List customers with filter and create a new customer:
+```
+            sysPass sysPassConn = new sysPass("http://sysPass.tld/sysPass/api.php", "ecbacg9d9c65c957a7h10d83dfe675e8ef27a912af6b4bcbaa5g0bcd8ie7b4aa", null, null);
+            ExpandoObject parameters = new ExpandoObject();
+            ((dynamic)parameters).name = "test";
+            dynamic responseObj = sysPassConn.objectResponse("getCustomers", parameters);
+            foreach (var item in responseObj.result)
+            {
+                if (item.Key.Equals("count", StringComparison.Ordinal))
+                {
+                    Console.WriteLine("Found a total of {0} customers", item.Value);
+                }else
+                {
+                    Console.WriteLine("Found customer {0} with description {1}", item.Value.customer_name, item.Value.customer_description);
+                }
+            }
+            parameters = new ExpandoObject();
+            ((dynamic)parameters).name = "testname";
+            ((dynamic)parameters).name = "testdescr";
+            responseObj = sysPassConn.objectResponse("addCustomer", parameters);
+```
+
+Example using sysPass.NET from Powershell:
+```
+
+```
+
+> **Note:**
+
+> - sysPass.NET uses Newtonsoft.Json library for json serialization and deserialization
+> - .NET 4.0, but you can compile to any version of .NET as long as it supports ExpandoObject and Newtonsoft.Json 
